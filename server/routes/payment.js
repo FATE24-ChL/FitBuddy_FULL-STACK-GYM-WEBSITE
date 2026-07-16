21de530aa480d9b94715b8a44f2205cbacaf0158
FATE24-ChL <chrislelouis2006@gmail.com>
2026-07-16 14:27:17 +0530

    Added payment verification

--- server/routes/payment.js
+++ server/routes/payment.js
@@ -1,5 +1,6 @@
 import express from "express";
 import Razorpay from "razorpay";
+import crypto from "crypto";
 import dotenv from 'dotenv';
 dotenv.config();
 const router = express.Router();
@@ -32,6 +33,39 @@ router.post("/create-order", async (req, res) => {
             message: "Unable to create order"
         });
     }
+});
+
+router.post("/verify-payment", (req, res) => {
+
+    const {
+        razorpay_order_id,
+        razorpay_payment_id,
+        razorpay_signature
+    } = req.body;
+
+    const body =
+        razorpay_order_id + "|" + razorpay_payment_id;
+
+    const expectedSignature = crypto
+        .createHmac(
+            "sha256",
+            process.env.RAZORPAY_KEY_SECRET
+        )
+        .update(body)
+        .digest("hex");
+
+    if (expectedSignature === razorpay_signature) {
+
+        return res.json({
+            success: true
+        });
+
+    }
+
+    return res.json({
+        success: false
+    });
+
 });
 
 export default router;
